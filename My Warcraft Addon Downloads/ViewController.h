dec4b3bdd67bd1d0239781a18f28643185fad16c
Josh Hegg <josh.hegg@gmail.com>
2015-07-22 20:21:28 -0700

    Initial Commit

---
+++ My Warcraft Addon Downloads/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  My Warcraft Addon Downloads
+//
+//  Created by jhegg on 7/22/15.
+//  Copyright (c) 2015 Josh Hegg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ My Warcraft Addon Downloads/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  My Warcraft Addon Downloads
+//
+//  Created by jhegg on 7/22/15.
+//  Copyright (c) 2015 Josh Hegg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
