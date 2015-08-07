cac5b3e3dfc0743f4b424ad5a1d5a7e25b93bc1a
Josh Hegg <josh.hegg@gmail.com>
2015-08-06 21:42:11 -0700

    Fit graph to view
    Also, add a detail label below the graph. Use constraints and auto layout so the graph and label fill the available space on any device width/height.

--- My Warcraft Addon Downloads/AddonGraphViewController.m
+++ My Warcraft Addon Downloads/AddonGraphViewController.m
@@ -10,7 +10,11 @@
 
 @interface AddonGraphViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *detailLabel;
 @property (weak, nonatomic) IBOutlet BEMSimpleLineGraphView *graph;
+@property NSArray *orderedHistory;
+@property NSDateFormatter *detailLabelDateFormatter;
+@property NSDateFormatter *xAxisDateFormatter;
 
 @end
 
@@ -19,8 +23,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self initializeHistory];
+    _detailLabelDateFormatter = [self buildDetailLabelDateFormatter];
+    _xAxisDateFormatter = [self buildXAxisDateFormatter];
+    
     _graph.enableTouchReport = YES;
     _graph.enablePopUpReport = YES;
+    _graph.enableYAxisLabel = YES;
+    _graph.enableXAxisLabel = YES;
+    _graph.autoScaleYAxis = YES;
+    _graph.alwaysDisplayDots = NO;
+    
+    _detailLabel.text = @"Touch the graph for details";
 }
 
 - (void)didReceiveMemoryWarning {
@@ -28,14 +42,43 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)initializeHistory {
+    NSSortDescriptor *sortByDateAscending = [[NSSortDescriptor alloc] initWithKey:@"timestamp" ascending:YES];
+    self.orderedHistory = [self.addon.downloadHistory sortedArrayUsingDescriptors:@[sortByDateAscending]];
+}
+
+- (NSDateFormatter *)buildDetailLabelDateFormatter {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    NSLocale *enUsPosixLocale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
+    [formatter setLocale:enUsPosixLocale];
+    [formatter setDateStyle:NSDateFormatterMediumStyle];
+    [formatter setTimeStyle:NSDateFormatterMediumStyle];
+    return formatter;
+}
+
+- (NSDateFormatter *)buildXAxisDateFormatter {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    NSLocale *enUsPosixLocale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
+    [formatter setLocale:enUsPosixLocale];
+    [formatter setDateStyle:NSDateFormatterMediumStyle];
+    return formatter;
+}
+
 - (NSInteger)numberOfPointsInLineGraph:(BEMSimpleLineGraphView * __nonnull)graph {
     return self.addon.downloadHistory.count;
 }
 
 - (CGFloat)lineGraph:(BEMSimpleLineGraphView * __nonnull)graph valueForPointAtIndex:(NSInteger)index {
-    NSSortDescriptor *sortByDateAscending = [[NSSortDescriptor alloc] initWithKey:@"timestamp" ascending:YES];
-    NSArray *orderedHistory = [self.addon.downloadHistory sortedArrayUsingDescriptors:@[sortByDateAscending]];
-    return [[[orderedHistory objectAtIndex:index] valueForKey:@"count"] floatValue];
+    return [[[self.orderedHistory objectAtIndex:index] valueForKey:@"count"] floatValue];
+}
+
+- (NSString *)lineGraph:(nonnull BEMSimpleLineGraphView *)graph labelOnXAxisForIndex:(NSInteger)index {
+    NSDate *date = [[self.orderedHistory objectAtIndex:index] objectForKey:@"timestamp"];
+    return [self.xAxisDateFormatter stringFromDate:date];
+}
+
+- (NSInteger)numberOfGapsBetweenLabelsOnLineGraph:(BEMSimpleLineGraphView * __nonnull)graph {
+    return self.addon.downloadHistory.count;
 }
 
 - (CGFloat)minValueForLineGraph:(BEMSimpleLineGraphView * __nonnull)graph {
@@ -44,6 +87,13 @@
 
 - (CGFloat)maxValueForLineGraph:(BEMSimpleLineGraphView * __nonnull)graph {
     return [[[self.addon.downloadHistory firstObject] valueForKey:@"count"] floatValue] * 1.10f;
+}
+
+- (void) lineGraph:(BEMSimpleLineGraphView * __nonnull)graph didTouchGraphWithClosestIndex:(NSInteger)index {
+    id dataPoint = [self.orderedHistory objectAtIndex:index];
+    NSDate *date = [dataPoint objectForKey:@"timestamp"];
+    NSNumber *count = [dataPoint objectForKey:@"count"];
+    self.detailLabel.text = [NSString stringWithFormat:@"%@ - %@", [self.detailLabelDateFormatter stringFromDate:date], count];
 }
 
 /*
